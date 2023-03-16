77ce9b2cbc539c89fb2fdc02e5df846be450a7e1
rdve <robbe.de.vilder@gmail.com>
2023-03-16 18:09:16 +0100

    .

--- main.js
+++ main.js
@@ -6,3 +6,12 @@ console.log('test3')
 console.log('test3')
 console.log('test3')
 console.log('test3')
+
+for(let i=0;i<5;i++){
+
+console.log('test3')
+console.log('test3')
+console.log('test3')
+console.log('test3')
+
+}
